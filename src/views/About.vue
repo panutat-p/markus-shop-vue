409a44aa4370552c55f3f165ccd60060ea0dffe4
panutat-p <pp.panutat@gmail.com>
2021-11-09 20:44:46 +0700

    complete About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,73 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="h1 text-success">Markus Shop</div>
+    <div class="h4 text-start mt-3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+      aliquid amet autem iste numquam placeat provident veniam? Delectus
+      distinctio ea enim est, fuga harum nihil porro quas quibusdam sapiente,
+      ullam!
+    </div>
+    <div class="h4 text-start mt-3">
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has
+      roots in a piece of classical Latin literature from 45 BC, making it over
+      2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+      College in Virginia, looked up one of the more obscure Latin words,
+      consectetur, from a Lorem Ipsum passage, and going through the cites of
+      the word in classical literature, discovered the undoubtable source. Lorem
+      Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+      Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
+      book is a treatise on the theory of ethics, very popular during the
+      Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+      amet..", comes from a line in section 1.10.32.
+    </div>
+    <div class="h4 text-start mt-3">
+      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below
+      for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus
+      Bonorum et Malorum" by Cicero are also reproduced in their exact original
+      form, accompanied by English versions from the 1914 translation by H.
+      Rackham.
+    </div>
+    <div class="h4 text-start mt-3">
+      It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout. The point of using
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+      as opposed to using 'Content here, content here', making it look like
+      readable English. Many desktop publishing packages and web page editors
+      now use Lorem Ipsum as their default model text, and a search for 'lorem
+      ipsum' will uncover many web sites still in their infancy. Various
+      versions have evolved over the years, sometimes by accident, sometimes on
+      purpose (injected humour and the like).
+    </div>
+    <figure>
+      <img
+        src="https://images.unsplash.com/photo-1553787434-45e1d245bfbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80"
+        alt="organic vegetables"
+      />
+    </figure>
+    <div class="h4 text-start mt-3">
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum."
+    </div>
+    <div class="h4 text-start mt-3">
+      "But I must explain to you how all this mistaken idea of denouncing
+      pleasure and praising pain was born and I will give you a complete account
+      of the system, and expound the actual teachings of the great explorer of
+      the truth, the master-builder of human happiness. No one rejects,
+      dislikes, or avoids pleasure itself, because it is pleasure, but because
+      those who do not know how to pursue pleasure rationally encounter
+      consequences that are extremely painful. Nor again is there anyone who
+      loves or pursues or desires to obtain pain of itself, because it is pain,
+      but because occasionally circumstances occur in which toil and pain can
+      procure him some great pleasure. To take a trivial example, which of us
+      ever undertakes laborious physical exercise, except to obtain some
+      advantage from it? But who has any right to find fault with a man who
+      chooses to enjoy a pleasure that has no annoying consequences, or one who
+      avoids a pain that produces no resultant pleasure?"
+    </div>
   </div>
 </template>
